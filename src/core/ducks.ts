b931076e4bed89e3af518e81407571f9732f61b8
Kamilo Vasquez <kamilo.vasquez@gamerquest.co>
2021-03-26 01:29:10 -0400

    Update ducks.ts

--- src/core/ducks.ts
+++ src/core/ducks.ts
@@ -2,13 +2,17 @@ import { combineReducers } from 'redux'
 import { all } from 'redux-saga/effects'
 import whoamiReducer from 'whoami/duck/reducer'
 import whoamiSagas from 'whoami/duck/sagas'
+import gamesReducer from 'games/duck/reducer'
+import gamesSagas from 'games/duck/sagas'
 
 export const reducers = combineReducers({
   whoami: whoamiReducer,
+  games: gamesReducer,
 })
 
 export function* sagas() {
   yield all([
     whoamiSagas(),
+    gamesSagas()
   ])
 }
